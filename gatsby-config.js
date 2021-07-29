f277321824dd4b7c855f4ad71e833f2b8c0faecf
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-07-29 16:37:06 +0100

    dynamic images added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,48 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: '@stefangouldson',
+    person: {name: 'John', age: 32},
+    simpleData: ['item 1', 'item 2'],
+    complexData: [
+      {name: 'John', age: 32},
+      {name: 'Susan', age: 21},
+    ]
+  },
+
   flags: {
     DEV_SSR: false
   },
+
   /* Your site config here */
   plugins: [
     "gatsby-plugin-styled-components",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `recipeImages`,
+        path: `${__dirname}/src/assets/images/recipes`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `styles`,
+        path: `${__dirname}/src/assets/css`,
+      },
+    },
   ],
 }
---
+++ src/pages/testing.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Gallery from "../components/Gallery"
+
+const Testing = () => {
+    return <Gallery />
+}
+
+export default Testing
